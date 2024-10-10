1d184769cf445a056e9babdf091f206ac1a43813
Hriday sen <hridaysen27@gmail.com>
2024-10-10 11:51:28 +0200

    The Golf Ball shows direction vector, and moves with realistic physics accross the golf panel.

--- src/Level1.java
+++ src/Level1.java
@@ -1,9 +1,13 @@
 
-import java.awt.Dimension;
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.*;  
 
-import javax.swing.JFrame;
+public class Level1 extends JPanel{
+    JFrame frame;
+    GolfBall ball;
 
-public class Level1 {
+
     public static void main(String[] args) {
         JFrame level1Frame = new JFrame("Level 1");
         level1Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -11,6 +15,7 @@ public class Level1 {
         level1Frame.setSize(new Dimension(600, 800));
         GolfBall golfBall = new GolfBall();
         level1Frame.add(golfBall);
-        level1Frame.setVisible(true);;
+        level1Frame.setVisible(true);
     }
+
 }
