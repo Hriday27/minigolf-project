bf925767da247d1137e48b9d8a11a6426c06f3bc
Hriday Sen <hridaysen27@gmail.com>
2024-10-15 15:32:03 +0200

    Revised Collision Logic for the golf ball

--- src/Hole.java
+++ src/Hole.java
@@ -4,7 +4,7 @@ import javax.swing.JPanel;
 
 public class Hole extends JPanel{
     @Override
-      public void paint(Graphics g){
+      public void paintComponent(Graphics g){
           g.setColor(Color.BLACK);
           g.fillOval(300,300, 30,30);
       }
--- src/Level1.java
+++ src/Level1.java
@@ -2,55 +2,36 @@
 import javax.swing.*; 
 import java.awt.*; 
 import java.awt.event.*;  
+import java.util.ArrayList;
 
 public class Level1 extends JPanel{
     JFrame frame;
-    GolfBall ball;
-
 
     public static void main(String[] args) {
         JFrame level1Frame = new JFrame("Level 1");
         level1Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        level1Frame.setSize(new Dimension(600, 800));
-        GolfBall golfBall = new GolfBall();
+        level1Frame.setSize(new Dimension(800, 800));
+        LevelDesigner level1 = new LevelDesigner();
 
-        level1Frame.add(golfBall);
-
+        level1Frame.add(level1);
 
         level1Frame.setVisible(true);
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(235);
+        list1.add(750);
+
+        // Second list
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(235);
+        list2.add(750);
+
+        if (list1.equals(list2)) {
+            System.out.println("The lists are equal." + list1 + " || " + list2);
+        } else {
+            System.out.println("The lists are not equal.");
+        }
 
     }
 
 }
-/*
- * public class Level1 {
-    public static void main(String[] args) {
-        JFrame level1Frame = new JFrame("Level 1");
-        level1Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        level1Frame.setSize(new Dimension(600, 800));
-
-        // Set OverlayLayout for stacking components
-        JPanel layer = new JPanel();
-        layer.setLayout(new OverlayLayout(layer));
-        level1Frame.add(layer);
-
-
-
-
-        Hole hole = new Hole();
-        layer.add(hole);
-        hole.setOpaque(false);
-
-        GolfBall golfBall = new GolfBall();
-        layer.add(golfBall);
-        golfBall.setOpaque(false); // Make sure thelayer is transparent
-
-
-
-        level1Frame.setVisible(true);
-    }
-
-}
- * 
- * 
- */
