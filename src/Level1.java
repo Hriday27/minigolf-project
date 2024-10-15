4ea5c8127129ecbb4301c3b3fae165a051fdb771
Hriday Sen <hridaysen27@gmail.com>
2024-10-15 16:19:17 +0200

    Fixed Rectangle Bottom Collision

--- src/Level1.java
+++ src/Level1.java
@@ -1,19 +1,37 @@
 
 import java.awt.*;
 import javax.swing.*; 
+import javax.swing.*;
+import javax.swing.text.JTextComponent;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
 public class Level1{
-    JFrame frame;
 
     public static void main(String[] args) {
-        JLayeredPane pane;
         JFrame level1Frame = new JFrame("Level 1");
         level1Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         level1Frame.setSize(new Dimension(800, 800));
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(800,800));
+
         LevelDesigner level1 = new LevelDesigner();
 
-        level1Frame.add(level1);
+        try {
+            BufferedImage background = ImageIO.read(new File("A:\\Eindhoven\\CBL Project\\Minigolf\\src\\golf.jpg"));
+            layeredPane.add(level1, JLayeredPane.PALETTE_LAYER);
 
+        } catch (Exception e) {
+        }
+
+        level1Frame.add(level1);
         level1Frame.setVisible(true);
 
     }
